1e6624c8257564a535d5a920e5de90ace3f313ac
davidcoats <david@getsafetycone.com>
2023-07-21 11:47:47 -0700

    Initial commit.

---
+++ source/R5T.T0222.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0222.Construction
+{
+	/// <summary>
+	/// Construction console project for the R5T.T0222 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0222/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0222
+{
+	/// <summary>
+	/// Organization related strong-types.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
